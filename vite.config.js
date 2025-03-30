f10905d9b72f53a35e0c94be37326a592c199be7
paranoiidandroid <fernandezmartin156@gmail.com>
2025-03-30 14:43:41 -0300

    Desarrollo de About, Contact y creación del componente Cards

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    base: "https://paranoiidandroid.github.io/prueba-react-tailwind/",
+    react()
+  ],
+  base: "https://paranoiidandroid.github.io/prueba-react-tailwind/",
+  resolve: {
+    alias: {
+      '@': path.resolve('src'), // Cambiado __dirname a la ruta relativa
+    },
+  },
 })
